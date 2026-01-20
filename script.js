e77bd9a6983edbf6f0a80fb35b1d71b6f58993d7
vanesangui8-commits <vanesangui8@gmail.com>
2026-01-20 22:08:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.writeln("<PRE>");
+document.write("<B><FONT SIZE=5>");
+document.writeln("SELAMAT DATANG DI JAVASCRIPT");
+document.write("</B></FONT>");
+document.write("<I>");
+document.writeln("Program ini merupakan contoh sederhana menampilkan Teks!");
+document.write("</I>");
+document.writeln("Dengan JavaScript!!!!!!");
